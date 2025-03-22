4a1261fa0b2909dbe22417a06b4889b9928ad503
tiawl <pabtomas@protonmail.com>
2025-03-22 19:05:13 +0100

    update to toolbox 1.12.0

--- build.zig
+++ build.zig
@@ -2,87 +2,75 @@ const std = @import("std");
 const toolbox = @import("toolbox");
 
 const Paths = struct {
-    // prefixed attributes
-    __glslang: []const u8 = undefined,
-    __glslang_in: []const u8 = undefined,
+    __glslang: []const u8,
+    __glslang_in: []const u8,
 
-    // mandatory getters
-    pub fn getGlslang(self: @This()) []const u8 {
+    fn getGlslang(self: @This()) []const u8 {
         return self.__glslang;
     }
-    pub fn getGlslangIn(self: @This()) []const u8 {
+
+    fn getGlslangIn(self: @This()) []const u8 {
         return self.__glslang_in;
     }
 
-    // mandatory init
-    pub fn init(builder: *std.Build) !@This() {
-        var self = @This(){
-            .__glslang = try builder.build_root.join(builder.allocator, &.{
-                "glslang",
-            }),
-        };
-
-        self.__glslang_in = try std.fs.path.join(builder.allocator, &.{
-            self.getGlslang(),
+    fn init() !@This() {
+        const glslang_path = try toolbox.instance().getBuilder().build_root.join(toolbox.instance().getBuilder().allocator, &.{
             "glslang",
         });
 
-        return self;
+        return .{
+            .__glslang = glslang_path,
+            .__glslang_in = toolbox.instance().ptrBuilder().pathJoin(&.{
+                glslang_path, "glslang",
+            }),
+        };
     }
 };
 
-fn update(builder: *std.Build, path: *const Paths, dependencies: *const toolbox.Dependencies) !void {
-    std.fs.deleteTreeAbsolute(path.getGlslang()) catch |err|
-        {
-            switch (err) {
-                error.FileNotFound => {},
-                else => return err,
-            }
-        };
+fn update(path: *const Paths, dependencies: *const toolbox.Dependencies) !void {
+    std.fs.deleteTreeAbsolute(path.getGlslang()) catch |err| {
+        switch (err) {
+            error.FileNotFound => {},
+            else => return err,
+        }
+    };
 
-    try dependencies.clone(builder, "glslang", path.getGlslang());
+    try dependencies.clone("glslang", path.getGlslang());
 
-    try toolbox.run(builder, .{
+    try toolbox.instance().run(.{
         .argv = &[_][]const u8{
             "python3",
-            try std.fs.path.join(builder.allocator, &.{
-                path.getGlslang(),
-                "build_info.py",
+            toolbox.instance().ptrBuilder().pathJoin(&.{
+                path.getGlslang(), "build_info.py",
             }),
             path.getGlslang(),
             "-i",
-            try std.fs.path.join(builder.allocator, &.{
-                path.getGlslang(),
-                "build_info.h.tmpl",
+            toolbox.instance().ptrBuilder().pathJoin(&.{
+                path.getGlslang(), "build_info.h.tmpl",
             }),
             "-o",
-            try std.fs.path.join(builder.allocator, &.{
-                path.getGlslangIn(),
-                "build_info.h",
+            toolbox.instance().ptrBuilder().pathJoin(&.{
+                path.getGlslangIn(), "build_info.h",
             }),
         },
     });
 
-    var glslang_dir =
-        try std.fs.openDirAbsolute(path.getGlslang(), .{
-            .iterate = true,
-        });
+    var glslang_dir = try std.fs.openDirAbsolute(path.getGlslang(), .{
+        .iterate = true,
+    });
     defer glslang_dir.close();
 
     var it = glslang_dir.iterate();
     while (try it.next()) |*entry| {
-        if (!std.mem.eql(u8, "SPIRV", entry.name) and
-            !std.mem.eql(u8, "StandAlone", entry.name) and
-            !std.mem.eql(u8, "glslang", entry.name))
-            try std.fs.deleteTreeAbsolute(try std.fs.path.join(builder.allocator, &.{
-                path.getGlslang(),
-                entry.name,
+        if (!std.mem.eql(u8, "SPIRV", entry.name) and !std.mem.eql(u8, "StandAlone", entry.name) and !std.mem.eql(u8, "glslang", entry.name)) {
+            try std.fs.deleteTreeAbsolute(toolbox.instance().ptrBuilder().pathJoin(&.{
+                path.getGlslang(), entry.name,
             }));
+        }
     }
 
-    const standalone_path = try std.fs.path.join(builder.allocator, &.{
-        path.getGlslang(),
-        "StandAlone",
+    const standalone_path = toolbox.instance().ptrBuilder().pathJoin(&.{
+        path.getGlslang(), "StandAlone",
     });
 
     var standalone_dir = try std.fs.openDirAbsolute(standalone_path, .{
@@ -92,14 +80,14 @@ fn update(builder: *std.Build, path: *const Paths, dependencies: *const toolbox.
 
     it = standalone_dir.iterate();
     while (try it.next()) |*entry| {
-        if (!toolbox.isCHeader(entry.name) and entry.kind == .file)
-            try std.fs.deleteFileAbsolute(try std.fs.path.join(builder.allocator, &.{
-                standalone_path,
-                entry.name,
+        if (!toolbox.isCHeader(entry.name) and entry.kind == .file) {
+            try std.fs.deleteFileAbsolute(toolbox.instance().ptrBuilder().pathJoin(&.{
+                standalone_path, entry.name,
             }));
+        }
     }
 
-    try toolbox.clean(builder, &.{
+    try toolbox.instance().clean(&.{
         "glslang",
     }, &.{});
 }
@@ -108,9 +96,9 @@ pub fn build(builder: *std.Build) !void {
     const target = builder.standardTargetOptions(.{});
     const optimize = builder.standardOptimizeOption(.{});
 
-    const path = try Paths.init(builder);
-
-    const dependencies = try toolbox.Dependencies.init(builder, .glslang_zig, "0xe15c80cea022542", &.{
+    toolbox.init(builder, optimize);
+    defer toolbox.deinit();
+    const dependencies = try toolbox.Dependencies.init(.glslang_zig, "0xe15c80cea022542", &.{
         "glslang",
     }, .{
         .toolbox = .{
@@ -126,43 +114,44 @@ pub fn build(builder: *std.Build) !void {
         },
     });
 
-    if (builder.option(bool, "update", "Update binding") orelse false)
-        try update(builder, &path, &dependencies);
+    const path = try Paths.init();
 
-    const lib = builder.addStaticLibrary(.{
+    if (toolbox.instance().ptrBuilder().option(bool, "update", "Update binding") orelse false) {
+        try update(&path, &dependencies);
+    }
+
+    const lib = toolbox.instance().ptrBuilder().addStaticLibrary(.{
         .name = "glslang",
-        .root_source_file = builder.addWriteFiles().add("empty.c", ""),
+        .root_source_file = toolbox.instance().ptrBuilder().addWriteFiles().add("empty.c", ""),
         .target = target,
         .optimize = optimize,
     });
 
     const flags = [_][]const u8{
-        "-DENABLE_HLSL",
-        "-fno-sanitize=undefined",
+        "-DENABLE_HLSL", "-fno-sanitize=undefined",
     };
 
     for ([_][]const u8{
         "glslang",
-        try std.fs.path.join(builder.allocator, &.{
-            "glslang",
-            "glslang",
+        toolbox.instance().ptrBuilder().pathJoin(&.{
+            "glslang", "glslang",
         }),
-        try std.fs.path.join(builder.allocator, &.{
-            "glslang",
-            "SPIRV",
+        toolbox.instance().ptrBuilder().pathJoin(&.{
+            "glslang", "SPIRV",
         }),
-        try std.fs.path.join(builder.allocator, &.{
-            "glslang",
-            "StandAlone",
+        toolbox.instance().ptrBuilder().pathJoin(&.{
+            "glslang", "StandAlone",
         }),
-    }) |include| toolbox.addInclude(lib, include);
+    }) |include| {
+        toolbox.instance().addInclude(lib, include);
+    }
 
-    toolbox.addHeader(lib, path.getGlslangIn(), "glslang", &.{
+    toolbox.instance().addHeader(lib, path.getGlslangIn(), "glslang", &.{
         ".h",
     });
-    toolbox.addHeader(lib, try std.fs.path.join(builder.allocator, &.{
-        path.getGlslang(),
-        "SPIRV",
+
+    toolbox.instance().addHeader(lib, toolbox.instance().ptrBuilder().pathJoin(&.{
+        path.getGlslang(), "SPIRV",
     }), "SPIRV", &.{
         ".h",
     });
@@ -174,7 +163,7 @@ pub fn build(builder: *std.Build) !void {
     });
     defer glslang_dir.close();
 
-    var walker = try glslang_dir.walk(builder.allocator);
+    var walker = try glslang_dir.walk(toolbox.instance().getBuilder().allocator);
     defer walker.deinit();
 
     walk: while (try walker.next()) |*entry| {
@@ -184,8 +173,9 @@ pub fn build(builder: *std.Build) !void {
                 while (it.next()) |*component| {
                     if (std.mem.eql(u8, component.name, "OSDependent")) continue :walk;
                 }
-                if (toolbox.isCppSource(entry.basename))
-                    try toolbox.addSource(lib, path.getGlslang(), entry.path, &flags);
+                if (toolbox.isCppSource(entry.basename)) {
+                    try toolbox.instance().addSource(lib, path.getGlslang(), entry.path, &flags);
+                }
             },
             else => {},
         }
@@ -197,10 +187,8 @@ pub fn build(builder: *std.Build) !void {
         else => return error.UnsupportedOs,
     };
 
-    const os_path = try std.fs.path.join(builder.allocator, &.{
-        path.getGlslangIn(),
-        "OSDependent",
-        os,
+    const os_path = toolbox.instance().ptrBuilder().pathJoin(&.{
+        path.getGlslangIn(), "OSDependent", os,
     });
 
     var os_dir = try std.fs.openDirAbsolute(os_path, .{
@@ -213,12 +201,13 @@ pub fn build(builder: *std.Build) !void {
     while (try it.next()) |*entry| {
         switch (entry.kind) {
             .file => {
-                if (toolbox.isCppSource(entry.name))
-                    try toolbox.addSource(lib, os_path, entry.name, &flags);
+                if (toolbox.isCppSource(entry.name)) {
+                    try toolbox.instance().addSource(lib, os_path, entry.name, &flags);
+                }
             },
             else => {},
         }
     }
 
-    builder.installArtifact(lib);
+    toolbox.instance().ptrBuilder().installArtifact(lib);
 }
