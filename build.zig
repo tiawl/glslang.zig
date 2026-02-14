8395b2c7aadb12726892a63b8b4e2a8a90cd8a32
tiawl <pabtomas@protonmail.com>
2026-02-14 19:55:50 +0100

    update with current toolbox rework state

--- build.zig
+++ build.zig
@@ -1,277 +1,111 @@
 const std = @import("std");
-const toolbox_pkg = @import("toolbox");
-const Toolbox = toolbox_pkg.Toolbox;
+const build_zig_zon = @import("build.zig.zon");
+const toolbox = @import("toolbox");
+const VerboseBuilder = toolbox.VerboseBuilder;
 
-const Paths = struct {
-    __glslang: []const u8,
-    __glslang_in: []const u8,
+fn updateFn(pkg_builder: *VerboseBuilder) !void {
+    try pkg_builder.remove(&.{"glslang"});
+    try pkg_builder.make(&.{"glslang"});
+    try pkg_builder.make(&.{ "glslang", "glslang" });
+    try pkg_builder.make(&.{ "glslang", "SPIRV" });
+    try pkg_builder.make(&.{ "glslang", "StandAlone" });
 
-    fn getGlslang(self: @This()) []const u8 {
-        return self.__glslang;
-    }
+    const glslang_dep = pkg_builder.verboseDependency("glslang");
+    var glslang_builder = VerboseBuilder.initFromDependency(glslang_dep);
 
-    fn getGlslangIn(self: @This()) []const u8 {
-        return self.__glslang_in;
-    }
+    _ = try glslang_builder.run(&.{ "python3", "build_info.py", ".", "-i", "build_info.h.tmpl", "-o", pkg_builder.resolve(&.{ "glslang", "build_info.h" }) }, glslang_builder.ptrCwd().*);
+    _ = try glslang_builder.run(&.{ "python3", "gen_extension_headers.py", ".", "-i", pkg_builder.resolve(&.{ "glslang", "ExtensionHeaders" }), "-o", pkg_builder.resolve(&.{ "glslang", "glsl_intrinsic_header.h" }) }, glslang_builder.ptrCwd().*);
 
-    fn init(toolbox: *Toolbox) !@This() {
-        const glslang_path = try toolbox.buildRootJoin(&.{
-            "glslang",
-        });
-
-        return .{
-            .__glslang = glslang_path,
-            .__glslang_in = toolbox.pathJoin(&.{
-                glslang_path, "glslang",
-            }),
-        };
-    }
-};
-
-fn update(toolbox: *Toolbox, path: *const Paths) !void {
-    std.fs.deleteTreeAbsolute(path.getGlslang()) catch |err| {
-        switch (err) {
-            error.FileNotFound => {},
-            else => return err,
-        }
-    };
-
-    try toolbox.clone(.glslang, path.getGlslang());
-
-    try toolbox.run(.{
-        .argv = &[_][]const u8{
-            "python3",
-            toolbox.pathJoin(&.{
-                path.getGlslang(), "build_info.py",
-            }),
-            path.getGlslang(),
-            "-i",
-            toolbox.pathJoin(&.{
-                path.getGlslang(), "build_info.h.tmpl",
-            }),
-            "-o",
-            toolbox.pathJoin(&.{
-                path.getGlslangIn(), "build_info.h",
-            }),
-        },
-    });
-
-    try toolbox.run(.{
-        .argv = &[_][]const u8{
-            "python3",
-            toolbox.pathJoin(&.{
-                path.getGlslang(), "gen_extension_headers.py",
-            }),
-            "-i",
-            toolbox.pathJoin(&.{
-                path.getGlslangIn(), "ExtensionHeaders",
-            }),
-            "-o",
-            toolbox.pathJoin(&.{
-                path.getGlslangIn(), "glsl_intrinsic_header.h",
-            }),
-        },
-    });
-
-    var glslang_dir = try std.fs.openDirAbsolute(path.getGlslang(), .{
-        .iterate = true,
-    });
-    defer glslang_dir.close();
-
-    var it = glslang_dir.iterate();
-    while (try it.next()) |*entry| {
-        if (!std.mem.eql(u8, "SPIRV", entry.name) and !std.mem.eql(u8, "StandAlone", entry.name) and !std.mem.eql(u8, "glslang", entry.name)) {
-            try std.fs.deleteTreeAbsolute(toolbox.pathJoin(&.{
-                path.getGlslang(), entry.name,
-            }));
+    for ([_][]const u8{ "glslang", "SPIRV", "StandAlone" }) |dir| {
+        while (try glslang_builder.walk(&.{dir})) |entry| {
+            switch (entry.kind) {
+                .file => if (toolbox.isCOrCpp11File(entry.basename)) {
+                    try pkg_builder.copy(&.{ "glslang", dir, entry.path }, &glslang_builder, &.{ dir, entry.path });
+                },
+                .directory => try pkg_builder.make(&.{ "glslang", dir, entry.path }),
+                else => {},
+            }
         }
     }
-
-    const standalone_path = toolbox.pathJoin(&.{
-        path.getGlslang(), "StandAlone",
-    });
-
-    var standalone_dir = try std.fs.openDirAbsolute(standalone_path, .{
-        .iterate = true,
-    });
-    defer standalone_dir.close();
-
-    it = standalone_dir.iterate();
-    while (try it.next()) |*entry| {
-        if (std.mem.startsWith(u8, entry.name, "StandAlone")) continue;
-        if (!toolbox_pkg.isCHeader(entry.name) and entry.kind == .file) {
-            try std.fs.deleteFileAbsolute(toolbox.pathJoin(&.{
-                standalone_path, entry.name,
-            }));
-        }
-    }
-
-    try toolbox.clean(&.{
-        "glslang",
-    }, &.{});
 }
 
-const FromZon = toolbox_pkg.Repositories(.{
-    .toolbox,
-});
+fn buildFn(pkg_builder: *VerboseBuilder) !void {
+    const lib = pkg_builder.addLibrary("glslang");
 
-const DuringExec = toolbox_pkg.Repositories(.{
-    .glslang,
-});
+    const flags = [_][]const u8{ "-DENABLE_HLSL", "-fno-sanitize=undefined" };
 
-pub fn build(builder: *std.Build) !void {
-    const target = builder.standardTargetOptions(.{});
-    const optimize = builder.standardOptimizeOption(.{});
+    pkg_builder.addInclude(lib, &.{"glslang"});
+    pkg_builder.addInclude(lib, &.{ "glslang", "glslang" });
+    pkg_builder.addInclude(lib, &.{ "glslang", "SPIRV" });
+    pkg_builder.addInclude(lib, &.{ "glslang", "StandAlone" });
 
-    var toolbox = try Toolbox.init(FromZon, DuringExec, builder, optimize, .glslang_zig, "0xe15c80cea022542", &.{
-        "glslang",
-    }, .{
-        .toolbox = .{
-            .name = "tiawl/toolbox",
-            .host = .github,
-            .ref = .tag,
-        },
-    }, .{
-        .glslang = .{
-            .name = "KhronosGroup/glslang",
-            .host = .github,
-            .ref = .commit,
-        },
-    });
-    defer toolbox.deinit();
-
-    const path = try Paths.init(&toolbox);
-
-    if (toolbox.getUpdate()) try update(&toolbox, &path);
-
-    const lib = builder.addLibrary(.{
-        .name = "glslang",
-        .root_module = std.Build.Module.create(builder, .{
-            .root_source_file = builder.addWriteFiles().add("empty.zig", ""),
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-
-    const flags = [_][]const u8{
-        "-DENABLE_HLSL", "-fno-sanitize=undefined",
-    };
-
-    for ([_][]const u8{
-        "glslang",
-        builder.pathJoin(&.{
-            "glslang", "glslang",
-        }),
-        builder.pathJoin(&.{
-            "glslang", "SPIRV",
-        }),
-        builder.pathJoin(&.{
-            "glslang", "StandAlone",
-        }),
-    }) |include| {
-        toolbox.addInclude(lib, include);
+    for ([_][]const u8{ "glslang", "SPIRV" }) |dir| {
+        while (try pkg_builder.walk(&.{ "glslang", dir })) |*entry| {
+            if (toolbox.isCHeader(entry.basename)) pkg_builder.installHeader(lib, &.{ "glslang", dir, entry.path }, &.{entry.path});
+        }
     }
 
-    toolbox.addHeader(lib, path.getGlslangIn(), "glslang", &.{
-        ".h",
-    });
+    pkg_builder.linkLibCpp(lib);
 
-    toolbox.addHeader(lib, builder.pathJoin(&.{
-        path.getGlslang(), "SPIRV",
-    }), "SPIRV", &.{
-        ".h",
-    });
-
-    lib.linkLibCpp();
-
-    var glslang_dir = try std.fs.openDirAbsolute(path.getGlslang(), .{
-        .iterate = true,
-    });
-    defer glslang_dir.close();
-
-    var walker = try glslang_dir.walk(builder.allocator);
-    defer walker.deinit();
-
-    walk: while (try walker.next()) |*entry| {
+    while (try pkg_builder.walk(&.{"glslang"})) |entry| {
         switch (entry.kind) {
             .file => {
-                var it = try std.fs.path.componentIterator(entry.path);
-                while (it.next()) |*component| {
-                    if (std.mem.eql(u8, component.name, "OSDependent")) continue :walk;
+                if (std.fs.path.dirname(entry.path)) |dirname| {
+                    var it = try std.fs.path.componentIterator(dirname);
+                    if (std.mem.eql(u8, it.next().?.name, "glslang")) {
+                        if (it.next()) |component| {
+                            if (std.mem.eql(u8, component.name, "OSDependent")) continue;
+                        }
+                    }
                 }
-                if (std.mem.startsWith(u8, entry.basename, "StandAlone")) continue :walk;
-                if (toolbox_pkg.isCppSource(entry.basename)) {
-                    try toolbox.addSource(lib, path.getGlslang(), entry.path, &flags);
+                if (std.mem.eql(u8, entry.basename, "StandAlone.cpp")) continue;
+                if (toolbox.isCppSource(entry.basename)) {
+                    pkg_builder.addCSource(lib, &.{ "glslang", entry.path }, &flags);
                 }
             },
             else => {},
         }
     }
 
-    const os = switch (target.result.os.tag) {
+    const os = switch (pkg_builder.getOs()) {
         .linux => "Unix",
         .windows => "Windows",
         else => return error.UnsupportedOs,
     };
 
-    const os_path = builder.pathJoin(&.{
-        path.getGlslangIn(), "OSDependent", os,
-    });
-
-    var os_dir = try std.fs.openDirAbsolute(os_path, .{
-        .iterate = true,
-    });
-    defer os_dir.close();
-
-    var it = os_dir.iterate();
-
-    while (try it.next()) |*entry| {
+    while (try pkg_builder.iterate(&.{ "glslang", "glslang", "OSDependent", os })) |entry| {
         switch (entry.kind) {
             .file => {
-                if (toolbox_pkg.isCppSource(entry.name)) {
-                    try toolbox.addSource(lib, os_path, entry.name, &flags);
+                if (toolbox.isCppSource(entry.name)) {
+                    pkg_builder.addCSource(lib, &.{ "glslang", "glslang", "OSDependent", os, entry.name }, &flags);
                 }
             },
             else => {},
         }
     }
 
-    // the exe part is coming from https://github.com/Games-by-Mason/glslang-zig
-    const exe = builder.addExecutable(.{
-        .name = "glslangValidator",
-        .root_module = std.Build.Module.create(builder, .{
-            .target = target,
-            .optimize = optimize,
-        }),
-    });
-    exe.linkLibCpp();
-    exe.linkLibrary(lib);
-    exe.addCSourceFiles(.{
-        .root = .{
-            .cwd_relative = toolbox.pathJoin(&.{
-                path.getGlslang(), "StandAlone",
-            }),
-        },
-        .files = &.{
-            "StandAlone.cpp",
-        },
-        .flags = &[_][]const u8{
-            "-std=c++17", "-fno-exceptions", "-fno-exceptions",
-        },
-    });
-    exe.addIncludePath(.{
-        .cwd_relative = path.getGlslang(),
-    });
-    exe.addIncludePath(.{
-        .cwd_relative = path.getGlslangIn(),
-    });
-    exe.root_module.addCMacro("ENABLE_OPT", if (builder.option(bool, "enable-opt", "Enables spirv-opt capability if present") orelse true) "1" else "0");
-    builder.installArtifact(exe);
-    const glslang_run_cmd = builder.addRunArtifact(exe);
-    glslang_run_cmd.step.dependOn(builder.getInstallStep());
-    if (builder.args) |args| glslang_run_cmd.addArgs(args);
-    const glslang_run_step = builder.step("glslangValidator", "Run glslang");
-    glslang_run_step.dependOn(&glslang_run_cmd.step);
+    const validator = pkg_builder.addExecutable("glslangValidator");
+    pkg_builder.linkLibC(validator);
+    pkg_builder.linkLibrary(validator, lib);
+    pkg_builder.addCSource(validator, &.{ "glslang", "StandAlone", "StandAlone.cpp" }, &.{ "-std=c++17", "-fno-exceptions", "-fno-exceptions" });
+    pkg_builder.addInclude(validator, &.{"glslang"});
+    pkg_builder.addInclude(validator, &.{ "glslang", "glslang" });
+    pkg_builder.addCMacro(validator, "ENABLE_OPT", if (pkg_builder.option(bool, true, "enable-opt", "Enables spirv-opt capability if present")) "1" else "0");
+    pkg_builder.installArtifact(validator);
 
-    builder.installArtifact(lib);
+    const glslang_run_cmd = pkg_builder.addRunArtifact(validator);
+    pkg_builder.dependOn(&glslang_run_cmd.step, pkg_builder.getInstallStep());
+    pkg_builder.addArgs(glslang_run_cmd, pkg_builder.getArgs());
+    const glslang_run_step = pkg_builder.step("glslangValidator", "Run glslang");
+    pkg_builder.dependOn(glslang_run_step, &glslang_run_cmd.step);
+
+    pkg_builder.installArtifact(lib);
+}
+
+pub fn build(builder: *std.Build) !void {
+    var pkg_builder = try VerboseBuilder.init(builder, build_zig_zon, buildFn, updateFn);
+
+    try pkg_builder.fetch(build_zig_zon);
+    try pkg_builder.update();
+    try pkg_builder.build();
 }
